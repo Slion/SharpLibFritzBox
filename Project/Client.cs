3922e141867896b85d9ce67d4a53e4a874342f97
Stéphane Lenclud <github@lenclud.com>
2017-08-07 12:20:00 +0200

    Basic client and query test to login_sid.lua

---
+++ Project/Client.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLib.FritzBox
+{
+
+    [DataContract(Namespace = "")]
+    public class SessionInfo
+    {
+        [DataMember]
+        string SID { get; set; }
+
+        /// <summary>
+        /// Order attribute property is important as explained there: 
+        /// https://stackoverflow.com/questions/19989883/some-properties-are-not-being-deserialized-using-datacontractserializer
+        /// </summary>
+        [DataMember(Name = "Challenge", IsRequired = true, Order = 1)]
+        string Challenge { get; set; }
+
+        [DataMember(Name = "BlockTime", Order = 2)]
+        string BlockTime { get; set; }
+        //string Rights { get; set; }
+    }
+
+
+    public class Client : HttpClient
+    {
+        public Client()
+        {
+            BaseAddress = new Uri("http://fritz.box/");
+            DefaultRequestHeaders.Accept.Clear();
+            DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+        }
+
+        public async Task<SessionInfo> GetSessionInfoAsync()
+        {
+            SessionInfo info = null;
+            HttpResponseMessage response = await GetAsync("login_sid.lua");
+            if (response.IsSuccessStatusCode)
+            {
+                //dynamic responseContent = await response.Content.ReadAsAsync<object>();
+                //string returnedToken = responseContent.Token;
+
+                //info = await response.Content.ReadAsAsync<SessionInfo>();
+
+                DataContractSerializer serializer = new DataContractSerializer(typeof(SessionInfo));
+                var stream = await response.Content.ReadAsStreamAsync();
+                info = serializer.ReadObject(stream) as SessionInfo;
+
+
+
+            }
+            return info;
+        }
+
+    }
+}
---
+++ Project/Demo/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FritzBox = SharpLib.FritzBox;
+
+namespace FritzBoxDemo
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void iButtonLogin_Click(object sender, EventArgs e)
+        {
+            FritzBox.Client client = new FritzBox.Client();
+            FritzBox.SessionInfo info = await client.GetSessionInfoAsync();
+        }
+    }
+}
