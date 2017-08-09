971257750afe2962a40cd33b54b46654dd4f3ba1
Stéphane Lenclud <github@lenclud.com>
2017-08-09 19:02:06 +0200

    SharpLibFritzBox Release v0.0.2

--- Project/Properties/AssemblyInfo.cs
+++ Project/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpLibFritzBox")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Slions")]
 [assembly: AssemblyProduct("SharpLibFritzBox")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
